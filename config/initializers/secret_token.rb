2ba20a0d86eb657823ba6ef4c01bb099a9f2a818
jmalmadclc <jmalmadclc@users.noreply.github.com>
2018-04-19 09:13:41 -0400

    Test this please.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3BootstrapDeviseCancan::Application.config.secret_token = 'a6cd68af2c0c93e2f0bd4c576aab509f59b7ae839d02018caa4c8ce3cc46ea012fdc4f1dc93bbb14137bc646fb697907e9649260d02b77eed838717d20dff479'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsQuickstart::Application.config.secret_key_base = '9673b19c0bc2ccb555138954771a7735629c5bd1a0b93744a95c88ea0f7a4b175feda6bfd0e2baccb48480d24279c0f0a22e58896e2d2ec78edaab660113ed28'
